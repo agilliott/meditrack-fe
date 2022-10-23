905f92a5bd2440cd489d4aea13fc1b8a06471457
Alexandria Gilliott <alexandriagilliott@outlook.com>
2022-10-23 14:48:49 +0100

    NumberSelect test

---
+++ src/components/NumberSelect.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '../test/test-utils';
+
+import NumberSelect from './NumberSelect';
+
+const defaultProps = {
+  value: 1,
+  selected: false,
+  callback: vi.fn(),
+};
+
+describe('<NumberSelect />', () => {
+  it('renders with default props', () => {
+    render(<NumberSelect {...defaultProps} />);
+
+    expect(
+      screen.getByRole('button', { name: defaultProps.value.toString() })
+    ).toBeInTheDocument();
+  });
+
+  it('executes callback on click', async () => {
+    const mockCallback = vi.fn();
+    const { user } = render(
+      <NumberSelect {...defaultProps} callback={mockCallback} />
+    );
+
+    await user.click(screen.getByRole('button'));
+
+    expect(mockCallback).toHaveBeenCalledWith(defaultProps.value);
+  });
+
+  it('shows selected styles', async () => {
+    render(<NumberSelect {...defaultProps} selected />);
+
+    expect(screen.getByRole('button')).toHaveStyle('border-color: #009a8a;');
+    expect(screen.getByRole('button')).toHaveStyle('color: #009a8a;');
+  });
+});
